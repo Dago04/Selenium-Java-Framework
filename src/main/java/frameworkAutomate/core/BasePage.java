5ab7343fd78fcd63a2b16b5f7a787d7e463b44ce
Dago Salas <DagoSalas1999@gmail.com>
2025-08-05 21:43:11 -0600

    Se cambia el idioma de los comentarios en basepage

--- src/main/java/frameworkAutomate/core/BasePage.java
+++ src/main/java/frameworkAutomate/core/BasePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 
-/** Superclase de todos los Page Objects. */
+/** Superclass of all Page Objects. */
 public abstract class BasePage {
 
 	protected WebDriver driver;
@@ -17,26 +17,26 @@ public abstract class BasePage {
 		PageFactory.initElements(driver, this);
 	}
 	
-	/* ---------- wrappers de interacción ---------- */
+	/* ---------- interaction wrappers ---------- */
 	
-	// Click en un elemento localizado por el By
+	// Click on an element located by the By
 	protected void click(By locator) {
 		waits.clickable(locator).click();
 	}
 	
-	// Escribir texto en un elemento localizado por el By
+	// Write text in an element located by the By
 	protected void write(By locator, String text) {
 		WebElement element = waits.visible(locator);
 		element.clear();
 		element.sendKeys(text);
 	}
 	
-	// Leer texto de un elemento localizado por el By
+	// Read text from an element located by the By
 	protected String read(By locator) {
 		return waits.visible(locator).getText();
 	}
 	
-	// Leer el título de la página actualAn
+	// Read the title of the current page
 	public String getTitle() {
 		return driver.getTitle();
 	}
