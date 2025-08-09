f9244e8317741727e0edf8193298f08f80f96c01
Dago Salas <DagoSalas1999@gmail.com>
2025-08-09 11:17:05 -0600

    Se agregan mas esperas globales a la clase wait además se agrega polliing para ignorar excepciones

--- src/main/java/frameworkAutomate/core/Waits.java
+++ src/main/java/frameworkAutomate/core/Waits.java
@@ -5,32 +5,137 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.*;
 import java.time.Duration;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
 public class Waits {
-	
+
 	private final WebDriver driver;
 	private final WebDriverWait wait;
-	
+	private final Duration defaultTimeout;
+	private final Duration defaultPolling;
+
 	public Waits(WebDriver driver) {
-		this.driver = driver;
-		int timeout = ConfigReader.getInt("explicit.timeout", 10);
-		this.wait = new WebDriverWait(driver, Duration.ofSeconds(timeout));
+		this.driver = Objects.requireNonNull(driver, "driver cannot be null");
+
+		int timeoutSec = ConfigReader.getInt("explicit.timeout", 10);
+		int pollingMs = ConfigReader.getInt("explicit.polling", 200);
+
+		this.defaultTimeout = Duration.ofSeconds(timeoutSec);
+		this.defaultPolling = Duration.ofMillis(pollingMs);
+
+		this.wait = new WebDriverWait(driver, defaultTimeout);
+
+		this.wait.pollingEvery((defaultPolling)).ignoring(NoSuchElementException.class)
+				.ignoring(StaleElementReferenceException.class).ignoring(ElementNotInteractableException.class);
 	}
 	
-	  /* ---------- Common Helpers ---------- */
+	 /** Create a temporal WebDriverWait with personalize time with the  polling/ignores. */
+    public WebDriverWait withTimeout(Duration timeout) {
+        WebDriverWait custom = new WebDriverWait(driver, timeout);
+        custom.pollingEvery(defaultPolling)
+              .ignoring(NoSuchElementException.class)
+              .ignoring(StaleElementReferenceException.class)
+              .ignoring(ElementClickInterceptedException.class)
+              .ignoring(ElementNotInteractableException.class);
+        return custom;
+    }
+	
+	private <T> T until(Function<WebDriver, T> condition) {
+        return wait.until(condition);
+    }
+
+	/* ---------- Basci waits ---------- */
 	public WebElement visible(By locator) {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+		return until(ExpectedConditions.visibilityOfElementLocated(locator));
 	}
-	
+
+	public WebElement present(By locator) {
+		return until(ExpectedConditions.presenceOfElementLocated(locator));
+	}
+
 	public WebElement clickable(By locator) {
-		return wait.until(ExpectedConditions.elementToBeClickable(locator));
+		return until(ExpectedConditions.elementToBeClickable(locator));
 	}
-	
+
+	public boolean invisible(By locator) {
+		return until(ExpectedConditions.invisibilityOfElementLocated(locator));
+	}
+
 	public boolean textPresent(By locator, String text) {
-		return wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+		return until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+	}
+
+	public List<WebElement> visibleAll(By locator) {
+		return until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
+
+	public List<WebElement> presenceOfAll(By locator) {
+		return until(ExpectedConditions.presenceOfAllElementsLocatedBy(locator));
+	}
+
+	public List<WebElement> numberOfElementsToBe(By locator, int size) {
+		return until(ExpectedConditions.numberOfElementsToBe(locator, size));
+	}
+
+	/*
+	 * ============================ Page / Navigation ============================
+	 */
+
+	public boolean documentReady() {
+		return until(d -> {
+			try {
+				return "complete".equals(((JavascriptExecutor) d).executeScript("return document.readyState"));
+			} catch (JavascriptException e) {
+				return false;
+			}
+		});
+	}
+
+	public boolean urlContains(String fragment) {
+
+		return until(ExpectedConditions.urlContains(fragment));
+	}
+
+	public boolean urlToBe(String url) {
+		return until(ExpectedConditions.urlToBe(url));
+	}
+
+	public boolean titleContains(String text) {
+		return until(ExpectedConditions.titleContains(text));
+	}
+
+	public boolean titleIs(String title) {
+		return until(ExpectedConditions.titleIs(title));
+	}
+
+	/*
+	 * ============================ Attributes/ State ============================
+	 */
+
+	public boolean attributeToBe(By locator, String name, String value) {
+		return until(ExpectedConditions.attributeToBe(locator, name, value));
+	}
+
+	public boolean attributeContains(By locator, String name, String value) {
+		return until(ExpectedConditions.attributeContains(locator, name, value));
+	}
+
+	public boolean stalenessOf(WebElement element) {
+		return until(ExpectedConditions.stalenessOf(element));
+	}
+
+	public boolean elementSelectionStateToBe(By locator, boolean selected) {
+		return until(ExpectedConditions.elementSelectionStateToBe(locator, selected));
 	}
 	
-	public List<WebElement> visibleAll(By locator) {
-		return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
-	}
+	/* ============================ Frames / Alerts ============================ */
+
+    public WebDriver frameToBeAvailableAndSwitchToIt(By locator) {
+        return until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(locator));
+    }
+
+    public Alert alertIsPresent() {
+        return until(ExpectedConditions.alertIsPresent());
+    }
 }
